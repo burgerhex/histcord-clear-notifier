9b131096d140c7b6793befa00f3dca34efb55a12
burgerhex <40219421+burgerhex@users.noreply.github.com>
2026-02-07 00:23:20 -0800

    Refined map name trimming logic

--- src/notifier/helpers.py
+++ src/notifier/helpers.py
@@ -4,7 +4,9 @@ import itertools
 # returns trimmed map name and clear type (most likely "[C]" or "[FC]")
 def trim_map_name(map_name):
     # get rid of author and weird newlines
-    map_name_no_author = map_name.replace("\n", " ").replace("  ", " ").split(" by ")[0]
+    map_name_no_newlines = map_name.replace("\n", " ")
+    map_name_no_double_spaces = " ".join(map_name_no_newlines.split())
+    map_name_no_author = map_name_no_double_spaces.split(" by ")[0]
     clear_type = "[C]"  # default
     trimmed_map_name = map_name_no_author
     if map_name_no_author.endswith("]"):
