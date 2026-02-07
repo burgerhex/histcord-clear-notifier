e6a61981529aaec326f14ca4e85ca7ef621f24cc
burgerhex <40219421+burgerhex@users.noreply.github.com>
2026-02-06 23:21:37 -0800

    Fix comment

--- src/notifier/helpers.py
+++ src/notifier/helpers.py
@@ -1,7 +1,7 @@
-# returns trimmed map name and clear type (most likely "[C]" or "[FC]")
 import itertools
 
 
+# returns trimmed map name and clear type (most likely "[C]" or "[FC]")
 def trim_map_name(map_name):
     # get rid of author and weird newlines
     map_name_no_author = map_name.replace("\n", " ").replace("  ", " ").split(" by ")[0]
